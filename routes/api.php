ab360dc0358c61839fb30c865c607c659d7ad545
Brenier Arnaud <arnaud.brenier91@gmail.com>
2024-04-27 10:14:01 +0200

    Add : implémentation de l'authentification

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 //*********
 // Public endpoints
@@ -14,3 +17,23 @@ Route::get('/ping', function () {
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    /** @var \App\Models\User $user */
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ])
+        ->status(401);
+    }
+
+    return $user->createToken("default")->plainTextToken;
+})
+->name('sanctum.createToken');
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\Administrateur;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\PersonalAccessToken;
 
 uses(RefreshDatabase::class);
 
@@ -22,4 +24,51 @@ test('user is not an admin', function () {
         ->create();
 
     expect($user->isAdmin())->toBeFalse();
+});
+
+test('user can authenticate', function() {
+
+    $user = User::factory()
+        ->create(['password' => Hash::make('password')]);
+
+    $response = $this->postJson('/api/sanctum/token',
+        [
+            'email' => $user->email,
+            'password' => 'password',
+        ]
+    );
+
+    $response->assertOk();
+
+    // Test token valide
+    $token = $response->getContent();
+    $persistedToken = PersonalAccessToken::findToken($token)->first();
+    $userToken = $persistedToken->tokenable;
+    expect($user->id)->toBe($userToken->id);
+});
+
+test('user cant authenticate', function() {
+
+    $user = User::factory()
+        ->create(['password' => Hash::make('password')]);
+
+    // wrong password
+    $response = $this->postJson('/api/sanctum/token',
+        [
+            'email' => $user->email,
+            'password' => 'wrong_password',
+        ]
+    );
+
+    $response->assertStatus(401);
+
+    //wrong user
+    $response = $this->postJson('/api/sanctum/token',
+        [
+            'email' => 'test@test.com',
+            'password' => 'password',
+        ]
+    );
+
+    $response->assertStatus(401);
 });
