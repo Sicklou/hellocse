93e5a26f7aa78d6398084a210023afbc2fabf98c
Brenier Arnaud <arnaud.brenier91@gmail.com>
2024-04-27 10:20:18 +0200

    upd : refacto acces token

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        /** @var \App\Models\User $user */
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ])
+            ->status(401);
+        }
+
+        return $user->createToken("default")->plainTextToken;
+    }
+}
